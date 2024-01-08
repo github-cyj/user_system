7d83aebaebd971622cd93ac13fe94883b088fe1a
xa_caoyj <xa_caoyj@xiannew.sunseer.co.jp>
2024-01-08 16:59:56 +0800

    fix gorm自定义类型

--- app/baseType/jsonTime.go
+++ app/baseType/jsonTime.go
@@ -1,6 +1,7 @@
 package baseType
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"time"
 )
@@ -11,4 +12,13 @@ type JsonTime time.Time
 func (jsonTime JsonTime) MarshalJSON() ([]byte, error) {
 	var stamp = fmt.Sprintf("\"%s\"", time.Time(jsonTime).Format("2006-01-02 15:04:05"))
 	return []byte(stamp), nil
+}
+
+func (jsonTime *JsonTime) Scan(value interface{}) error {
+	*jsonTime = JsonTime(value.(time.Time))
+	return nil
+}
+
+func (jsonTime JsonTime) Value() (driver.Value, error) {
+	return time.Time(jsonTime), nil
 }

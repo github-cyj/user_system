2852b3b80c1e24c32e5e45862a13979bec9412b8
xa_caoyj <xa_caoyj@xiannew.sunseer.co.jp>
2024-01-08 09:57:09 +0800

    时间格式调整

--- app/baseType/jsonTime.go
+++ app/baseType/jsonTime.go
@@ -1,9 +1,7 @@
 package baseType
 
 import (
-	"encoding/json"
 	"fmt"
-	"gorm.io/gorm"
 	"time"
 )
 
@@ -13,15 +11,4 @@ type JsonTime time.Time
 func (t JsonTime) MarshalJSON() ([]byte, error) {
 	var stamp = fmt.Sprintf("\"%s\"", time.Time(t).Format("2006-01-02 15:04:05"))
 	return []byte(stamp), nil
-}
-
-type JsonDeleteTime gorm.DeletedAt
-
-// MarshalJSON 实现它的json序列化方法
-func (t JsonDeleteTime) MarshalJSON() ([]byte, error) {
-	if t.Valid {
-		var stamp = fmt.Sprintf("\"%s\"", t.Time.Format("2006-01-02 15:04:05"))
-		return []byte(stamp), nil
-	}
-	return json.Marshal(nil)
 }
--- app/models/baseModel.go
+++ app/models/baseModel.go
@@ -7,10 +7,10 @@ import (
 )
 
 type BaseModel struct {
-	ID         uint                    `gorm:"primaryKey"  json:"id"`
-	CreateTime baseType.JsonTime       `gorm:"autoCreateTime" json:"create_time" go:"unix"`
-	UpdateTime baseType.JsonTime       `gorm:"autoUpdateTime" json:"update_time" format_time:"unix"`
-	DeleteTime baseType.JsonDeleteTime `gorm:"index;->:false" json:"-" format:"unix"`
+	ID         uint              `gorm:"primaryKey"  json:"id"`
+	CreateTime baseType.JsonTime `gorm:"autoCreateTime" json:"create_time" go:"unix"`
+	UpdateTime baseType.JsonTime `gorm:"autoUpdateTime" json:"update_time" format_time:"unix"`
+	DeleteTime gorm.DeletedAt    `gorm:"index" json:"-" format:"unix"`
 }
 
 func NewDb() *gorm.DB {
